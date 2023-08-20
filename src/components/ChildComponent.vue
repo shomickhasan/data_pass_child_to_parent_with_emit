5c572b777cf04739f403d79f7720ce1a2182e36f
Shomick_Hasan <shomick15-12158@diu.edu.bd>
2023-08-20 21:58:25 +0600

    first commit

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+  message: String,
+});
+
+defineEmits(['ChildMessage'])
+
+
+</script>
+
+<template>
+  <div class="container p-4 border rounded">
+    <h2 class="text-lg font-semibold mb-2">Child Component</h2>
+    <p class="mb-2">{{ props.message }}</p>
+    <button @click="$emit('ChildMessage','Hi this is message form child!!')" class="btn btn-primary">Emit Message</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,22 @@
+<script setup>
+import ChildComponent from "@/components/ChildComponent.vue";
+import {ref} from "vue";
+const message = ref('Hello from Parent!');
+const ChildMessage= (msg)=>{
+  message.value= msg
+
+}
+
+
+</script>
+
+<template>
+  <div class="container p-4">
+    <h1 class="text-xl font-bold mb-4">Parent Component</h1>
+   <ChildComponent :message="message" @ChildMessage="ChildMessage"/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
